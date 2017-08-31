657f1a06f9f0446a2eb719cd114291113b853603
cbrusch <crystal_brusch@brown.edu>
2017-08-31 16:35:06 -0400

    setting the left / right handle boundaries

--- demos/image-comparison-slider/app/js/before-after.js
+++ demos/image-comparison-slider/app/js/before-after.js
@@ -6,21 +6,31 @@
 			
 			dragElement.addClass('draggable');
 			resizeElement.addClass('resizable');
+			// create separate selectors for left and right handles
+			if(dragElement.hasClass('handle__left') ) {
+				dragElement.addClass('draggable__left'),
+				resizeElement.addClass('resizable__left')
+			}
+			else if(dragElement.hasClass('handle__right') ) {
+				dragElement.addClass('draggable__right'),
+				resizeElement.addClass('resizable__right')
+			}
 
 			// Check if it's a mouse or touch even and pass along the correct value
 			var startX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
 
 			// Get the initial position
 			var dragWidth = dragElement.outerWidth(),
-				posX = dragElement.offset().left + dragWidth - startX,
+				posXHandleLeft = $('.draggable__left').offset().left + dragWidth - startX,
+				posXHandleRight = $('.draggable__right').offset().left + dragWidth - startX,
 				containerOffset = container.offset().left,
 				containerWidth = container.outerWidth();
 
 			// Set limits for left handle
 			minLeftHandleLeft = containerOffset + 10;
-			maxLeftHandleLeft = containerOffset + containerWidth - dragWidth - 10;
+			maxLeftHandleLeft = containerOffset + containerWidth - posXHandleRight - dragWidth - 10;
 			// Set limits for right handle
-			minLeftHandleRight = containerOffset + 10;
+			minLeftHandleRight = containerOffset + posXHandleLeft + 10;
 			maxLeftHandleRight = containerOffset + containerWidth - dragWidth - 10;
 
 			// Calculate the dragging distance on mousemove or touchmove events
@@ -30,12 +40,22 @@
 				var moveX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
 					leftValue = moveX + posX - dragWidth;
 
-				// Prevent going out of bounds
-				if( leftValue < minLeft ) {
-					leftValue = minLeft;
+				// Prevent left and right handles from going out of bounds
+				if( dragElement.hasClass('draggable__left') ) {
+					if( leftValue < minLeftHandleLeft ) {
+						leftValue = minLeftHandleLeft;
+					}
+					else if( leftValue > maxLeftHandleLeft ) {
+						leftValue = maxLeftHandleLeft;
+					}
 				}
-				else if( leftValue > maxLeft ) {
-					leftValue = maxLeft;
+				else if( dragElement.hasClass('draggable__right') ) {
+					if( leftValue < minLeftHandleRight ) {
+						leftValue = minLeftHandleRight;
+					}
+					else if( leftValue > maxLeftHandleRight ) {
+						leftValue = maxLeftHandleRight;
+					}
 				}
 
 				// Translate the handle's left value to the masked div's width
@@ -73,81 +93,6 @@
 		});
 	}
 }(jQuery));
-
-
-
-
-// (function($) {
-//   function drags(dragElement, resizeElement, container) {
-    
-//     // Initialize the dragging event on mousedown.
-//     dragElement.on('mousedown.ba-events touchstart.ba-events', function(e) {
-      
-//       dragElement.addClass('ba-draggable');
-//       resizeElement.addClass('ba-resizable');
-      
-//       // Check if it's a mouse or touch event and pass along the correct value
-//       var startX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
-      
-//       // Get the initial position
-//       var dragWidth = dragElement.outerWidth(),
-//           posX = dragElement.offset().left + dragWidth - startX,
-//           containerOffset = container.offset().left,
-//           containerWidth = container.outerWidth();
-   
-//       // Set limits
-//       minLeft = containerOffset + 10;
-//       maxLeft = containerOffset + containerWidth - dragWidth - 10;
-      
-//       // Calculate the dragging distance on mousemove.
-//       dragElement.parents().on("mousemove.ba-events touchmove.ba-events", function(e) {
-        
-//         // Check if it's a mouse or touch event and pass along the correct value
-//         var moveX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
-        
-//         leftValue = moveX + posX - dragWidth;
-        
-//         // Prevent going off limits
-//         if ( leftValue < minLeft) {
-//           leftValue = minLeft;
-//         } else if (leftValue > maxLeft) {
-//           leftValue = maxLeft;
-//         }
-        
-//         // Translate the handle's left value to masked divs width.
-//         widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
-        
-//         // Set the new values for the slider and the handle. 
-//         $('.ba-draggable').css('left', widthValue);
-//         $('.ba-resizable').css('width', widthValue);
-//       // Bind mouseup events to stop dragging.
-//       }).on('mouseup.ba-events touchend.ba-events touchcancel.ba-events', function(){
-//         dragElement.removeClass('ba-draggable');
-//         resizeElement.removeClass('ba-resizable');
-//         // Unbind all events for performance
-//         $(this).off('.ba-events'); 
-//       });
-//       e.preventDefault();
-//     });
-//   }
-
-//   // Define plugin
-//   $.fn.beforeAfter = function() {
-//     var cur = this;
-//     // Adjust the slider
-//     var width = cur.width()+'px';
-//     cur.find('.resize img').css('width', width);
-//     // Bind dragging events
-//     drags(cur.find('.handle'), cur.find('.resize'), cur);
-
-//     // Update sliders on resize. 
-//     // Because we all do this: i.imgur.com/YkbaV.gif
-//     $(window).resize(function(){
-//       var width = cur.width()+'px';
-//       cur.find('.resize img').css('width', width);
-//     });
-//   }
-// }(jQuery));
 
 
 
